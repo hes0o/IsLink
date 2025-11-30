51b141e807fbc2832acb3e94a73558276ef10e3e
Hassan Chawa <hassanchawa@Hassans-MacBook-Air.local>
2025-12-01 00:54:55 +0300

    fix: Remove duplicate BrowserRouter from App.jsx
    - BrowserRouter was already in main.jsx, causing routing issues
    - Kept only Routes and Route in App.jsx

--- islink-frontend/src/App.jsx
+++ islink-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout';
 import Home from './pages/Home';
 import './styles/variables.css';
@@ -6,14 +6,12 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          {/* More routes will be added here */}
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* More routes will be added here */}
+      </Route>
+    </Routes>
   );
 }
 
