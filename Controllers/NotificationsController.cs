bb6c588c8928935635f91d8889d6c1ec08038c8c
Ahmed Al Hamed <133318802+AHMEDMOM1@users.noreply.github.com>
2025-12-06 17:46:37 +0300

    Create  NotificationsController.cs

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,22 @@
+[Authorize]
+[ApiController]
+[Route("api/notifications")]
+public class NotificationsController : ControllerBase
+{
+    private readonly INotificationRepository _repo;
+    private readonly IMapper _mapper;
+
+    public NotificationsController(INotificationRepository repo, IMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<NotificationDto>>> GetMyNotifications()
+    {
+        var userId = int.Parse(User.FindFirst("userId")!.Value);
+        var notifications = await _repo.GetForUserAsync(userId);
+        return _mapper.Map<List<NotificationDto>>(notifications);
+    }
+}
