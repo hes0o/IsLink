ad0112f324117d8ba48168726e723cffa5cafe8e
Ahmed Al Hamed <133318802+AHMEDMOM1@users.noreply.github.com>
2025-12-07 18:53:22 +0300

    Update OrderRepository.cs

--- Repositories/OrderRepository.cs
+++ Repositories/OrderRepository.cs
@@ -2,6 +2,9 @@ using FreelancerPlatform.Data;
 using FreelancerPlatform.Entities;
 using FreelancerPlatform.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FreelancerPlatform.Repositories
 {
@@ -17,11 +20,39 @@ namespace FreelancerPlatform.Repositories
         {
             return await _context.Orders
                 .Include(o => o.Client)
-                .ThenInclude(u => u.Profile)
+                    .ThenInclude(u => u.Profile)
                 .Include(o => o.Freelancer)
+                    .ThenInclude(u => u.Profile)
                 .Include(o => o.Gig)
                 .Include(o => o.Review)
                 .FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersForClientAsync(int clientId)
+        {
+            return await _context.Orders
+                .Include(o => o.Gig)
+                .Include(o => o.Client).ThenInclude(u => u.Profile)
+                .Include(o => o.Freelancer).ThenInclude(u => u.Profile)
+                .Where(o => o.ClientId == clientId)
+                .OrderByDescending(o => o.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersForFreelancerAsync(int freelancerId)
+        {
+            return await _context.Orders
+                .Include(o => o.Gig)
+                .Include(o => o.Client).ThenInclude(u => u.Profile)
+                .Include(o => o.Freelancer).ThenInclude(u => u.Profile)
+                .Where(o => o.FreelancerId == freelancerId)
+                .OrderByDescending(o => o.CreatedAt)
+                .ToListAsync();
+        }
+
+        public void AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
         }
 
         public async Task<bool> SaveChangesAsync()
